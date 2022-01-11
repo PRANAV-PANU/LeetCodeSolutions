d6ee066df0e8c1e99fdd573385037162ebbaca1f
Pranav Agarwal <pranav.juit@gmail.com>
2022-01-11 16:37:49 +0530

    Update 1022. Sum of Root To Leaf Binary Numbers.cpp

--- JanuaryChallenge/1022. Sum of Root To Leaf Binary Numbers.cpp
+++ JanuaryChallenge/1022. Sum of Root To Leaf Binary Numbers.cpp
@@ -1,1 +1,28 @@
-
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    int findLeaf(TreeNode* root,int sum){
+        if(!root)
+            return 0;
+        
+        sum = 2*sum+root->val;
+        if(!root->left && !root->right)
+            return sum;
+        
+        return findLeaf(root->left,sum)+findLeaf(root->right,sum);
+    }
+    
+    int sumRootToLeaf(TreeNode* root) {
+        return findLeaf(root,0);
+    }
+};
